96c46ea0cb5501d5b9ef2c7fcd1dcca262048090
ByteSizeCode <you@yourdomain.com>
2020-05-10 22:47:47 -0700

    Added more colors and made structure more modular for ease of customizing number of colors

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This is a simple demonstration of dragging and dropping colors to set the backgr
 <br />
 <br />
 <img src="Demo.gif" width="400" alt="drawing"/>
-
+-------------------
 Usage:
 Drag to cell, then click cell after drop to keep the color there. No click will not keep.
+
--- SwiftUI Drag Colors/ContentView.swift
+++ SwiftUI Drag Colors/ContentView.swift
@@ -11,9 +11,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            ColorSource(color: .purple, color2: .orange, lastColorDragged: .none)
+            ColorSource(colors: [.purple,.orange, .yellow, .green, .gray, .clear], lastColorDragged: .none)
             Spacer()
-            ColorSource(color: .blue, color2: .pink, lastColorDragged: .none)
+            ColorSource(colors: [.blue,.pink, .gray, .purple, .white, .black], lastColorDragged: .none)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
@@ -47,42 +47,55 @@ struct DestinationDataSetter: View {
     }
 }
 
+enum colorsDragged {
+    case first, second, third, fourth, fifth, sixth, none
+}
+
 struct DestinationView: View {
     @Binding var active: Int
     let label: String
     let id: Int
-    let color: Color
-    let color2: Color
+    let colors: [Color]
     @Binding var permanentColoring:[Color]
     @Binding var lastColorDragged:colorsDragged
 
     var body: some View {
-        Text(label).padding(10).background( ((self.active == id) ) ? (self.lastColorDragged == .first ? color : (self.lastColorDragged == .second ? color2 : .yellow)) : self.permanentColoring[self.id])
+        Text(label).padding(10).background( ((self.active == id) ) ? (self.lastColorDragged == .first ? colors[0] : (self.lastColorDragged == .second ? colors[1] : (self.lastColorDragged == .third ? colors[2] : (self.lastColorDragged == .fourth ? colors[3] : (self.lastColorDragged == .fifth ? colors[4] : (self.lastColorDragged == .sixth ? colors[5] : .clear)))))) : self.permanentColoring[self.id])
                 .background(DestinationDataSetter(destination: id))
             .onTapGesture {
                 if(self.lastColorDragged == .first) {
-                    print("First color dragged: \(self.color) and not \(self.color2)")
-                    self.permanentColoring[self.id] = self.color
+//                    print("First color dragged: \(self.colors[0]) and not \(self.colors[1])")
+                    self.permanentColoring[self.id] = self.colors[0]
                     print(self.permanentColoring[self.id])
                 }
                 else if(self.lastColorDragged == .second){
-                    print("Second color dragged: \(self.color2) and not \(self.color)")
-                    self.permanentColoring[self.id] = self.color2
+                    self.permanentColoring[self.id] = self.colors[1]
+                    print(self.permanentColoring[self.id])
+                }
+                else if(self.lastColorDragged == .third){
+                    self.permanentColoring[self.id] = self.colors[2]
+                    print(self.permanentColoring[self.id])
+                }
+                else if(self.lastColorDragged == .fourth){
+                    self.permanentColoring[self.id] = self.colors[3]
+                    print(self.permanentColoring[self.id])
+                }
+                else if(self.lastColorDragged == .fifth){
+                    self.permanentColoring[self.id] = self.colors[4]
+                    print(self.permanentColoring[self.id])
+                }
+                else if(self.lastColorDragged == .sixth){
+                    self.permanentColoring[self.id] = self.colors[5]
                     print(self.permanentColoring[self.id])
                 }
         }
     }
 }
 
-enum colorsDragged {
-    case first, second, none
-}
-
 struct ColorSource: View {
     @State var active = 0
     @State var destinations: [Int: CGRect] = [:]
-    @State var color:Color
-    @State var color2:Color
+    @State var colors:[Color]
     @State var draggingFirstColor:Bool = false
     @State var draggingSecondColor:Bool = false
     @State var lastColorDragged:colorsDragged
@@ -91,50 +104,138 @@ struct ColorSource: View {
     var body: some View {
         VStack {
             
-            Text("Drag Color From Here").padding().background(color)
-                .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
-                    .onChanged { value in
-                        self.draggingFirstColor = true
-                        self.lastColorDragged = .first
-                        self.active = 0
-                        for (id, frame) in self.destinations {
-                            if frame.contains(value.location) {
-                                self.active = id
-                            }
+            HStack(spacing: 5) {
+                VStack(spacing: 5) {
+                    Text("Drag Color From Here").padding().background(self.colors[0])
+                        .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
+                            .onChanged { value in
+                                self.draggingFirstColor = true
+                                self.lastColorDragged = .first
+                                self.active = 0
+                                for (id, frame) in self.destinations {
+                                    if frame.contains(value.location) {
+                                        self.active = id
+                                    }
+                                }
                         }
-                    }
-                    .onEnded { value in
-                        // do something on drop
-//                        self.active = 0
-//                        self.draggingFirstColor = false
-                        
-                    }
-            )
-            
-            Text("Drag Color From Here").padding().background(color2)
-                .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
-                    .onChanged { value in
-                        self.draggingSecondColor = true
-                        self.lastColorDragged = .second
-                        self.active = 0
-                        for (id, frame) in self.destinations {
-                            if frame.contains(value.location) {
-                                self.active = id
+                        .onEnded { value in
+                            // do something on drop
+                            //                        self.active = 0
+                            //                        self.draggingFirstColor = false
+                            
                             }
+                    )
+                    
+                    Text("Drag Color From Here").padding().background(colors[1])
+                        .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
+                            .onChanged { value in
+                                self.draggingSecondColor = true
+                                self.lastColorDragged = .second
+                                self.active = 0
+                                for (id, frame) in self.destinations {
+                                    if frame.contains(value.location) {
+                                        self.active = id
+                                    }
+                                }
                         }
-                    }
-                    .onEnded { value in
-                        // do something on drop
-//                        self.active = 0
-//                         self.draggingSecondColor = false
-                    }
-            )
+                        .onEnded { value in
+                            // do something on drop
+                            //                        self.active = 0
+                            //                         self.draggingSecondColor = false
+                            }
+                    )
+                }
+                VStack(spacing: 5) {
+                    Text("Drag Color From Here").padding().background(self.colors[2])
+                        .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
+                            .onChanged { value in
+                                self.draggingFirstColor = true
+                                self.lastColorDragged = .third
+                                self.active = 0
+                                for (id, frame) in self.destinations {
+                                    if frame.contains(value.location) {
+                                        self.active = id
+                                    }
+                                }
+                        }
+                        .onEnded { value in
+                            // do something on drop
+                            //                        self.active = 0
+                            //                        self.draggingFirstColor = false
+                            
+                            }
+                    )
+                    
+                    Text("Drag Color From Here").padding().background(colors[3])
+                        .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
+                            .onChanged { value in
+                                self.draggingSecondColor = true
+                                self.lastColorDragged = .fourth
+                                self.active = 0
+                                for (id, frame) in self.destinations {
+                                    if frame.contains(value.location) {
+                                        self.active = id
+                                    }
+                                }
+                        }
+                        .onEnded { value in
+                            // do something on drop
+                            //                        self.active = 0
+                            //                         self.draggingSecondColor = false
+                            }
+                    )
+                }
+                VStack(spacing: 5) {
+                    Text("Drag Color From Here").padding().background(self.colors[4])
+                        .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
+                            .onChanged { value in
+                                self.draggingFirstColor = true
+                                self.lastColorDragged = .fifth
+                                self.active = 0
+                                for (id, frame) in self.destinations {
+                                    if frame.contains(value.location) {
+                                        self.active = id
+                                    }
+                                }
+                        }
+                        .onEnded { value in
+                            // do something on drop
+                            //                        self.active = 0
+                            //                        self.draggingFirstColor = false
+                            
+                            }
+                    )
+                    
+                    Text("Delete Color Drag This").padding().background(colors[5])
+                     .border(Color.gray)
+                        .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .global)
+                            .onChanged { value in
+                                self.draggingSecondColor = true
+                                self.lastColorDragged = .sixth
+                                self.active = 0
+                                for (id, frame) in self.destinations {
+                                    if frame.contains(value.location) {
+                                        self.active = id
+                                    }
+                                }
+                        }
+                        .onEnded { value in
+                            // do something on drop
+                            //                        self.active = 0
+                            //                         self.draggingSecondColor = false
+                            }
+                    )
+                }
+            }
             
             Divider()
-            DestinationView(active: $active, label: "Drag A Color Here", id: 1, color: self.color, color2: self.color2, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
-            DestinationView(active: $active, label: "Drag A Color Here", id: 2, color: self.color, color2: self.color2, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
-            DestinationView(active: $active, label: "Drag A Color Here", id: 3, color: self.color, color2: self.color2, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
-            DestinationView(active: $active, label: "Drag A Color Here", id: 4, color: self.color, color2: self.color2, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
+            DestinationView(active: $active, label: "Drag A Color Here", id: 1, colors: self.colors, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
+            DestinationView(active: $active, label: "Drag A Color Here", id: 2, colors: self.colors, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
+
+            DestinationView(active: $active, label: "Drag A Color Here", id: 3, colors: self.colors, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
+
+            DestinationView(active: $active, label: "Drag A Color Here", id: 4, colors: self.colors, permanentColoring: self.$permanentColoring, lastColorDragged: self.$lastColorDragged)
+
         }.onPreferenceChange(DestinationDataKey.self) { preferences in
             for p in preferences {
                 self.destinations[p.destination] = p.frame
